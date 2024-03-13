99637aa89456b5f41ee067b41b35fb86ff169a43
Josh McGlynn <x21358616@student.ncirl.ie>
2024-03-13 19:05:08 +0000

    Test commit and added .gitignore for netbeans files

--- src/playlistApp/DataStructAlgorithmsCA1.java
+++ src/playlistApp/DataStructAlgorithmsCA1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package datastruct.algorithmsca1;
+package playlistApp;
 
 /**
  *
@@ -12,9 +12,8 @@ public class DataStructAlgorithmsCA1 {
 
     /**
      * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     */    public static void main(String[] args) {
+           //test commit 
     }
     
 }
